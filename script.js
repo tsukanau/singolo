2bc654ce5bfb41a93ae03a3c197b8ea36dec2121
tsukanau <tsukanau@yandex.ru>
2020-03-20 19:31:40 +0300

    feat: add singolo task with DOM elements modifying three files

--- script.js
+++ script.js
@@ -1,33 +1,90 @@
+//header
 const MENU = document.getElementById('menu');
 
-MENU.addEventListener('click', (event) => {
-    MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
-    event.target.classList.add('active');
+function moveToAnchor() {
+    if (window.pageYOffset > '601' && window.pageYOffset < '1105') {
+        MENU.querySelectorAll('.navigationItem')
+            .forEach(el => el.classList.remove('active'));
+        MENU.querySelector('#navigationServ').classList.add('active');
+}   else if (window.pageYOffset > '-1' && window.pageYOffset < '600') {
+    MENU.querySelectorAll('.navigationItem')
+        .forEach(el => el.classList.remove('active'));
+    MENU.querySelector('#navigationHome').classList.add('active');
+}   else if (window.pageYOffset > '1106' && window.pageYOffset < '1959') {
+    MENU.querySelectorAll('.navigationItem')
+        .forEach(el => el.classList.remove('active'));
+    MENU.querySelector('#navigationPort').classList.add('active');
+}   else if (window.pageYOffset > '1960' && window.pageYOffset < '2705') {
+    MENU.querySelectorAll('.navigationItem')
+        .forEach(el => el.classList.remove('active'));
+    MENU.querySelector('#navigationAbout').classList.add('active');
+}   else if (window.pageYOffset > '2705') {
+    MENU.querySelectorAll('.navigationItem')
+        .forEach(el => el.classList.remove('active'));
+    MENU.querySelector('#navigationCont').classList.add('active');
+}
+
+
+}
+window.addEventListener('scroll', () => moveToAnchor());
+
+//slider 
+let CHANGE = document.getElementById('section-slider')
+let items = document.querySelectorAll('.carousel .item');
+let currentItem = 0;
+let isEnabled = true;
+
+function clickArrow () {
+    if (CHANGE.classList.contains('section-slider-blue')) {
+        CHANGE.classList.remove('section-slider-blue');
+    } else CHANGE.classList.add('section-slider-blue');
+}
+
+function changeCurrentItem(n) {
+	currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+	isEnabled = false;
+	items[currentItem].classList.add(direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('activeslide', direction);
+	});
+}
+
+function showItem(direction) {
+	items[currentItem].classList.add('next', direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('next', direction);
+		this.classList.add('activeslide');
+		isEnabled = true;
+	});
+}
+
+function nextItem(n) {
+	hideItem('to-left');
+	changeCurrentItem(n + 1);
+    showItem('from-right');
+}
+
+function previousItem(n) {
+	hideItem('to-right');
+	changeCurrentItem(n - 1);
+	showItem('from-left');
+}
+
+document.querySelector('.control.left').addEventListener('click', function() {
+	if (isEnabled) {
+        previousItem(currentItem);
+	}
 });
 
-var slideIndex = 1;
-showSlides(slideIndex);
+document.querySelector('.control.right').addEventListener('click', function() {
+	if (isEnabled) {
+        nextItem(currentItem);
+	}
+});
 
-function plusSlides(n){
-    showSlides(slideIndex += n);
-}
-function currentSlide(n){
-    showSlides(slideIndex = n);
-}
-function showSlides(n){
-    var i;
-    var slides = document.getElementsByClassName('slide');
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex=slides.length
-    }
-    for (i=0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
 
 
 function touchPhoneVert () {
@@ -50,10 +107,13 @@ function touchPhoneHor () {
     }
 }
 
+//porfolio
+const portfolioButtonsElements = document.getElementById('portfolio-navBar');
 
-
-
-
+portfolioButtonsElements.addEventListener('click', (event) => {
+    portfolioButtonsElements.querySelectorAll('span').forEach(el => el.classList.remove('portfolio-button__active'));
+    event.target.classList.add('portfolio-button__active');
+});
 const PICTURES = document.getElementById('portfolio');
 
 PICTURES.addEventListener('click', (event) => {
@@ -61,36 +121,6 @@ PICTURES.addEventListener('click', (event) => {
     event.target.classList.add('active-frame');
 });
 
-
-
-const BUTTON = document.getElementById('button'); 
-const CLOSEBUTTON = document.getElementById('close-btn');
-
-BUTTON.addEventListener('click', () => {
-    const subject = document.getElementById('subject').value.toString();
-    document.getElementById('message-block').classList.remove('hidden');
-    if(Boolean (subject)) {
-        document.getElementById('subject-result').innerText = 'Singolo';
-    }
-    else {
-        document.getElementById('subject-result').innerText = 'Без темы';
-    }
-
-    const description = document.getElementById('description').value.toString();
-    if (Boolean (description)) {
-        document.getElementById('description-result').innerText = 'Portfolio project';
-    }
-    else {
-        document.getElementById('description-result').innerText = 'Без описания';
-    }
-
-});
-CLOSEBUTTON.addEventListener('click', () => {
-    document.getElementById('mySingoloForm').reset();
-    document.getElementById('message-block').classList.add('hidden');
-});
-
-const portfolioButtonsElements = document.getElementById('portfolio-navBar')
 const portfolioButtons = document.getElementById('portfolio-navBar').children;
 for(let i = 0; i < portfolioButtons.length; i++) {
     portfolioButtons[i].addEventListener('click', handleClickPortfolioButton);
@@ -114,8 +144,30 @@ function handleClickPortfolioButton() {
         itemsContainer.append(sortedArray[i]);
     }
 }
+// Form GetQuote
+const BUTTON = document.getElementById('button'); 
+const CLOSEBUTTON = document.getElementById('close-btn');
 
-portfolioButtonsElements.addEventListener('click', (event) => {
-    portfolioButtonsElements.querySelectorAll('span').forEach(el => el.classList.remove('portfolio-button__active'));
-    event.target.classList.add('portfolio-button__active');
+BUTTON.addEventListener('click', () => {
+    const subject = document.getElementById('subject').value.toString();
+    document.getElementById('message-block').classList.remove('hidden');
+    if(Boolean (subject)) {
+        document.getElementById('subject-result').innerText = 'Subject: ' + subject;
+    }
+    else {
+        document.getElementById('subject-result').innerText = 'No subject';
+    }
+
+    const description = document.getElementById('description').value.toString();
+    if (Boolean (description)) {
+        document.getElementById('description-result').innerText = 'Description: ' + description;
+    }
+    else {
+        document.getElementById('description-result').innerText = 'No description';
+    }
+
+});
+CLOSEBUTTON.addEventListener('click', () => {
+    document.getElementById('mySingoloForm').reset();
+    document.getElementById('message-block').classList.add('hidden');
 });
