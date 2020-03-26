1835c29e440b402c441d74e6f0bed9233a06cea4
tsukanau <tsukanau@yandex.ru>
2020-03-26 13:21:48 +0300

    feat: add responsive design

--- script.js
+++ script.js
@@ -85,96 +85,6 @@ document.querySelector('.control.right').addEventListener('click', function() {
         nextItem(currentItem);
 	}
 });
-const swipedetect = (el) => {
-  
-	let surface = el;
-	let startX = 0;
-	let startY = 0;
-	let distX = 0;
-	let distY = 0;
-	let startTime = 0;
-	let elapsedTime = 0;
-
-	let threshold = 150;
-	let restraint = 100;
-	let allowedTime = 300;
-
-	surface.addEventListener('mousedown', function(e){
-		startX = e.pageX;
-		startY = e.pageY;
-		startTime = new Date().getTime();
-		e.preventDefault();
-	}, false);
-
-	surface.addEventListener('mouseup', function(e){
-		distX = e.pageX - startX;
-		distY = e.pageY - startY;
-		elapsedTime = new Date().getTime() - startTime;
-		if (elapsedTime <= allowedTime){
-			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
-				if ((distX > 0)) {
-					if (isEnabled) {
-						previousItem(currentItem);
-					}
-				} else {
-					if (isEnabled) {
-						nextItem(currentItem);
-					}
-				}
-			}
-		}
-		e.preventDefault();
-	}, false);
-
-	surface.addEventListener('touchstart', function(e){
-		if (e.target.classList.contains('arrow') || e.target.classList.contains('control')) {
-			if (e.target.classList.contains('left')) {
-				if (isEnabled) {
-					previousItem(currentItem);
-				}
-			} else {
-				if (isEnabled) {
-					nextItem(currentItem);
-				}
-			}
-		}
-			var touchobj = e.changedTouches[0];
-			startX = touchobj.pageX;
-			startY = touchobj.pageY;
-			startTime = new Date().getTime();
-			e.preventDefault();
-	}, false);
-
-	surface.addEventListener('touchmove', function(e){
-			e.preventDefault();
-	}, false);
-
-	surface.addEventListener('touchend', function(e){
-			var touchobj = e.changedTouches[0];
-			distX = touchobj.pageX - startX;
-			distY = touchobj.pageY - startY;
-			elapsedTime = new Date().getTime() - startTime;
-			if (elapsedTime <= allowedTime){
-					if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
-							if ((distX > 0)) {
-								if (isEnabled) {
-									previousItem(currentItem);
-								}
-							} else {
-								if (isEnabled) {
-									nextItem(currentItem);
-								}
-							}
-					}
-			}
-			e.preventDefault();
-	}, false);
-}
-
-var el = document.querySelector('.carousel');
-swipedetect(el);
-
-
 
 
 
@@ -306,6 +216,16 @@ MENU2.addEventListener('click', (event) => {
 	
 });
 
+COVER.addEventListener('click', () => {
+    if (OVERLAY.classList.contains('activeoverlay')) {
+        OVERLAY.classList.remove('activeoverlay');
+	} else OVERLAY.classList.add('activeoverlay');
+	
+	if (COVER.classList.contains('coveractive')) {
+        COVER.classList.remove('coveractive');
+	} else COVER.classList.add('coveractive');
+});
+	
 	
 
 
